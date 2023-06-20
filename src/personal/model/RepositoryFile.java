dc3fbeb880f5da709d5770551ac32c726865280b
«Tatyana <tasi-chka@yandex.ru>
2023-06-20 21:10:40 +0300

    Add

---
+++ src/personal/model/RepositoryFile.java
@@ -0,0 +1,92 @@
+package personal.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositoryFile implements Repository {
+    private UserMapper mapper = new UserMapper();
+    private FileOperation fileOperation;
+
+    public RepositoryFile(FileOperation fileOperation) {
+        this.fileOperation = fileOperation;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<String> lines = fileOperation.readAllLines();
+        List<User> users = new ArrayList<>();
+        for (String line : lines) {
+            users.add(mapper.map(line));
+        }
+        return users;
+    }
+
+    @Override
+    public String CreateUser(User user) {
+
+        List<User> users = getAllUsers();
+        int max = 0;
+        for (User item : users) {
+            int id = Integer.parseInt(item.getId());
+            if (max < id){
+                max = id;
+            }
+        }
+        int newId = max + 1;
+        String id = String.format("%d", newId);
+        user.setId(id);
+        users.add(user);
+        List<String> lines = mapToString(users);
+        fileOperation.saveAllLines(lines);
+        return id;
+    }
+
+    private List<String> mapToString(List<User> users) {
+        List<String> lines = new ArrayList<>();
+        for (User item: users) {
+            lines.add(mapper.map(item));
+        }
+        return lines;
+    }
+
+    public void deleteUser(User user){
+        List<String> lines = fileOperation.readAllLines();
+        List<User> users = new ArrayList<>();
+        for (String line : lines) {
+            users.add(mapper.map(line));
+        }
+
+        int findIndex = -1;
+
+        for (int i = 0; i < users.size(); i++) {
+            User currentUser = users.get(i);
+            if (user.getId().equals(currentUser.getId())){
+                findIndex = i;
+                break;
+            }
+        }
+
+        if (findIndex > -1) {
+            lines.remove(findIndex);
+        }
+
+        fileOperation.saveAllLines(lines);
+
+    }
+
+
+
+    @Override
+    public User updateUser(User user) {
+        List<User> users = getAllUsers();
+        for (User currentUser: users) {
+            if (currentUser.getId().equals(user.getId())){
+                currentUser.setFirstName(user.getFirstName());
+                currentUser.setLastName(user.getLastName());
+                currentUser.setPhone(user.getPhone());
+            }
+        }
+        fileOperation.saveAllLines(mapToString(users));
+        return user;
+    }
+}
---
+++ src/personal/views/ViewUser.java
@@ -0,0 +1,103 @@
+package personal.views;
+
+import personal.controllers.UserController;
+import personal.model.FileOperationImpl;
+import personal.model.User;
+import personal.views.validator.NameAndSurnameValidator;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ViewUser {
+
+    private UserController userController;
+
+    public ViewUser(UserController userController) {
+        this.userController = userController;
+    }
+
+    public void run() {
+        Commands com = Commands.NONE;
+
+        while (true) {
+            String command = prompt("Введите команду: ");
+            try {
+                com = Commands.valueOf(command.toUpperCase());
+                if (com == Commands.EXIT) return;
+                switch (com) {
+                    case CREATE:
+                        createUser();
+                        break;
+                    case READ:
+                        readUser();
+                        break;
+                    case LIST:
+                        readList();
+                        break;
+                    case UPDATE:
+                        updateUser();
+                        break;
+                    case DELETE:
+                        delete();
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void updateUser() throws Exception {
+        readList();
+        User user = getUser();
+        User updatedUser = getNewUser();
+        updatedUser.setId(user.getId());
+        User savedUser = userController.updateUser(updatedUser);
+        System.out.println(savedUser);
+    }
+    
+    private void delete() throws Exception{
+        String userid = prompt("Идентификатор пользователя: ");
+        userController.deleteUser(userid);
+    }
+
+    private void readList() {
+        List<User> listUsers = userController.readAllUsers();
+        for (User user: listUsers) {
+            System.out.println(user);
+        }
+    }
+
+    private void readUser() throws Exception {
+        User user = getUser();
+        System.out.println(user);
+    }
+
+    private User getUser() throws Exception {
+        String id = prompt("Идентификатор пользователя: ");
+        User user = userController.readUser(id);
+        return user;
+    }
+
+    private void createUser() throws Exception {
+        User user = getNewUser();
+        userController.saveUser(user);
+    }
+
+    private User getNewUser() throws Exception {
+        String firstName = prompt("Имя: ");
+        new NameAndSurnameValidator(firstName).validate();
+        String lastName = prompt("Фамилия: ");
+        new NameAndSurnameValidator(lastName).validate();
+        String phone = prompt("Номер телефона: ");
+        User user = new User(firstName, lastName, phone);
+        return user;
+    }
+
+
+    private String prompt(String message) {
+        Scanner in = new Scanner(System.in);
+        System.out.print(message);
+        return in.nextLine();
+    }
+}
